a2bd69e91aa00c358389cf6dec5883051eba2aaa
An2ans <antoniojesus_92@hotmail.com>
2022-04-06 20:38:00 +0100

    added submitform to deal with emails, added css for large devices, added more-techno page, other style fixes

---
+++ index.js
@@ -0,0 +1,12 @@
+//scrollreveal to reveal each section while scrolling down
+
+ScrollReveal().reveal(".header");
+ScrollReveal().reveal(".about", { delay: 500 });
+ScrollReveal().reveal(".whatIsTechno", { delay: 500 });
+ScrollReveal().reveal(".favArtists", { delay: 500 });
+ScrollReveal().reveal(".contact", { delay: 500 });
+
+ScrollReveal().reveal(".type2", { delay: 500 });
+ScrollReveal().reveal(".type3", { delay: 500 });
+ScrollReveal().reveal(".type4", { delay: 500 });
+ScrollReveal().reveal(".type5", { delay: 500 });
