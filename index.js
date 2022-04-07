4caa43b1a5e5ec214da2fc0e6dfd73eb7ec68f68
An2ans <antoniojesus_92@hotmail.com>
2022-04-07 20:18:07 +0100

    finish styles, changes on fav artists, ready to try gh pages and submit

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ ScrollReveal().reveal(".whatIsTechno", { delay: 500 });
 ScrollReveal().reveal(".favArtists", { delay: 500 });
 ScrollReveal().reveal(".contact", { delay: 500 });
 
+ScrollReveal().reveal(".type1");
 ScrollReveal().reveal(".type2", { delay: 500 });
 ScrollReveal().reveal(".type3", { delay: 500 });
 ScrollReveal().reveal(".type4", { delay: 500 });
 ScrollReveal().reveal(".type5", { delay: 500 });
+
+const copyright = document.querySelector(".copyright");
+
+const year = new Date().getFullYear();
+
+copyright.innerText = `Copyright © ${year} Antonio Beltrán`;
